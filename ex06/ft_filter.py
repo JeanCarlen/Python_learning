ba496f07b88e2eaad1fd44f94795115e18a5ce86
JeanCarlen <94684234+JeanCarlen@users.noreply.github.com>
2024-03-01 17:24:46 +0100

    Delete ex06 directory

--- ex06/ft_filter.py
+++
@@ -1,9 +0,0 @@
-def ft_filter(function, iterable):
-    if (iterable is None):
-        return None
-    if (function is None):
-        return [i for i in iterable if i]
-    return [i for i in iterable if function(i)]
-
-# passe une fonction sur chaque element de la liste(iterable)
-# et retourne une liste des elements qui ont retourné True
